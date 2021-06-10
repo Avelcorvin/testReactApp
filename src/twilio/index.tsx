d31897cffc6aabe85cef026d113679418cbce56e
korbanvitaliy <v.korban@skybuffer.com>
2021-06-10 15:13:15 +0300

    aded buttons

--- src/twilio/index.tsx
+++ src/twilio/index.tsx
@@ -1,9 +1,11 @@
 import { Device, Connection, PStream, PreflightTest } from 'twilio-client';
-import React, { EventHandler, useEffect, useState } from 'react'
+import React, { EventHandler, useEffect, useRef, useState } from 'react'
 
 
 export const TwilloApp = ()=>{
-    const tokenAdres = 'https://a168306c15b3.ngrok.io/voice/token'
+    const ref:{}|any = useRef(null)
+
+    const tokenAdres = '/voice/token'
     const device = new Device();
     console.log(device)
     
@@ -13,12 +15,24 @@ export const TwilloApp = ()=>{
             location: "Matrix"
         });
     }
+    const callBumber =()=>{
+        const number = ref.current.value;
+        console.log(number)
+
+        const connection = device.connect({
+         To: number
+        });
+        connection.on('ringing', function () {
+            console.log('Ringing...');
+          });
+
+    }
 
     const pressButton = (EE:any|EventListenerObject)=>{
         const value = EE.target.innerHTML;
-        console.log(value)
+        // console.log(value)
         const activeConnection =device.activeConnection();
-        console.log(activeConnection)
+        // console.log(activeConnection)
 
         activeConnection?.sendDigits(value)
     
@@ -46,14 +60,23 @@ export const TwilloApp = ()=>{
 
 
     return(
-        <p>
-            <button onClick={callToNodeJS}>Call to NodeJS</button>
-            <table>
-                <tr><td onMouseDown={pressButton}  className='number'>1</td ><td onMouseDown={pressButton} className='number'>2</td ><td onMouseDown={pressButton} className='number'>3</td ></tr>
-                <tr><td onMouseDown={pressButton} className='number'>4</td ><td onMouseDown={pressButton} className='number'>5</td ><td onMouseDown={pressButton} className='number'>6</td ></tr>
-                <tr><td onMouseDown={pressButton} className='number'>7</td ><td onMouseDown={pressButton} className='number'>8</td ><td onMouseDown={pressButton} className='number'>9</td ></tr>
-            </table>
-        </p>
+        <div className='twillo-container'>
+            <div className='twillo-ontainer_button' onClick={callToNodeJS} >Call to NodeJS</div>
+                <div>
+                    <table>
+                        <tr><td onMouseDown={pressButton}  className='number'>1</td ><td onMouseDown={pressButton} className='number'>2</td ><td onMouseDown={pressButton} className='number'>3</td ></tr>
+                        <tr><td onMouseDown={pressButton} className='number'>4</td ><td onMouseDown={pressButton} className='number'>5</td ><td onMouseDown={pressButton} className='number'>6</td ></tr>
+                        <tr><td onMouseDown={pressButton} className='number'>7</td ><td onMouseDown={pressButton} className='number'>8</td ><td onMouseDown={pressButton} className='number'>9</td ></tr>
+                        <tr><td onMouseDown={pressButton} className='number'>*</td ><td onMouseDown={pressButton} className='number'>0</td ><td onMouseDown={pressButton} className='number'>=</td ></tr>
+                    </table>
+                </div>
+            {/* <input ref={ref} className='twillo-container_inputnumber'></input>
+            <div 
+                className='twillo-ontainer_button'
+                onClick={callBumber}>
+                    Call Number
+            </div> */}
+        </div>
     )
 
 }
